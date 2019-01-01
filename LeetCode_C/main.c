8489d404177998f62ae51c64ff5f9753d887c9bc
GDUT-Rp <1132767421@qq.com>
2019-01-01 17:50:26 +0800

    C版本

---
+++ LeetCode_C/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+#include "LeetCode3.h"
+
+void LeetCode3_begin(){
+    char str[50] = "abcabcbb";
+    printf("%d\n", LeetCode3(str));
+};
+
+int main() {
+    printf("Hello, World!\n");
+    LeetCode3_begin();
+    return 0;
+}
