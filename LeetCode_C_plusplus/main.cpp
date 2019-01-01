0311eb928ec2a3560e6bf584cd4333d63e35655b
GDUT-Rp <1132767421@qq.com>
2019-01-01 17:31:26 +0800

    C++实现版

---
+++ LeetCode_C_plusplus/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <set>
+
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int ans = 0, i = 0, j = 0;
+        set<char> charset;
+        int length = s.length();
+        while (i < length && j < length){
+            if (!charset.count(s[j])){
+                charset.insert(s[j++]);
+                ans = max(ans, j - i);
+            } else{
+                charset.erase(s[i++]);
+            }
+        }
+        return ans;
+    }
+
+public:
+    int improveByVector(string s){
+        int length = s.length();
+        int ans = 0, i = 0, j = 0;
+        vector<int> index(128);
+        for (; j < length ; j++) {
+            i = max(index[s[j]], i);
+            ans = max(ans, j - i + 1);
+            index[s[j]] = j + 1;
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+    string s = "abcabcbb";
+    Solution solution;
+    cout << solution.lengthOfLongestSubstring(s) << endl;
+    cout << solution.improveByVector(s) << endl;
+    return 0;
+}
